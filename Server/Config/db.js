86aafce7363acf26a86f32a360bfe54d2eb22bd4
Irinnahar <irincse.bu@gmail.com>
2021-06-21 14:10:57 -0400

    connect to db

--- Server/Config/db.js
+++ Server/Config/db.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Secret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
-exports.LocalURI = "mongodb://localhost/books229";
-exports.RemoteURI = "your remote host information goes here";
+exports.LocalURI = "mongodb+srv://admin:yN9RJl5eF62kMK3h@comp229.kuasm.mongodb.net/bookStore?retryWrites=true&w=majority";
+exports.RemoteURI = "RemoteHost";
 exports.HostName = "LocalHost";
 exports.Secret = "someSecret";
 //# sourceMappingURL=db.js.map
--- Server/Config/db.ts
+++ Server/Config/db.ts
@@ -1,4 +1,4 @@
-export const LocalURI = "mongodb://localhost/books229";
-export const RemoteURI = "your remote host information goes here"
+export const LocalURI = "mongodb+srv://admin:yN9RJl5eF62kMK3h@comp229.kuasm.mongodb.net/bookStore?retryWrites=true&w=majority";
+export const RemoteURI = "RemoteHost"
 export const HostName = "LocalHost";
 export const Secret = "someSecret";
